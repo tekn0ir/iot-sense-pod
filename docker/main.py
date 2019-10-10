b6789bb2271467f74ea01b04995e1163fa83ebe8
Anders Åslund <anders.aslund@teknoir.se>
2019-10-10 14:40:19 +0200

    Remove publish callback Decode payload

--- docker/main.py
+++ docker/main.py
@@ -37,31 +37,34 @@ def error_str(rc):
     return '{}: {}'.format(rc, mqtt.error_string(rc))
 
 def on_connect(_client, _userdata, _flags, rc):
-    logger.info('on_connect {}'.format(error_str(rc)))
+    logger.info('Connected to internal broker {}'.format(error_str(rc)))
 
-def on_publish(_client, _userdata, _mid):
-    logger.info('on_publish')
+# def on_publish(_client, _userdata, _mid):
+#     logger.info('on_publish')
 
 def on_message(_client, _userdata, msg):
-    payload = str(msg.payload)
+    payload = msg.payload.decode("utf-8","ignore")
     logger.info('Received on topic \'{}\' payload \'{}\' '.format(msg.topic, payload))
     parsed_json = json.loads(payload)
+    logger.info('Parsed: \'{}\''.format(parsed_json))
     if 'color' in parsed_json:
+        logger.info('Color: \'{}\''.format(color))
         global color
         c = parsed_json.get('color', {'r': 0, 'g': 0, 'b': 0})
         color = (c.get('r', 0), c.get('g', 0), c.get('b', 0))
     if 'message' in parsed_json:
         global message
         message = parsed_json.get('message')
+        logger.info('Message: \'{}\''.format(message))
         with animate_lock:
             sense.show_message(message, text_colour=color)
 
 client.on_connect = on_connect
-client.on_publish = on_publish
+# client.on_publish = on_publish
 client.on_message = on_message
 
 client.connect(os.getenv("HMQ_SERVICE_HOST", "hmq.kube-system"), os.getenv("HMQ_SERVICE_PORT", 1883))
-client.subscribe("toe/config", qos=1)
+client.subscribe("toe/commands/iot-sense-pod/#", qos=1)
 client.loop_start()  # This runs the network code in a background thread and also handles reconnecting for you.
 
 tl = Timeloop()
@@ -84,3 +87,4 @@ def animation():
         sense.set_pixels(rainbow.pixels)
 
 tl.start(block=True)
+sense.clear()
